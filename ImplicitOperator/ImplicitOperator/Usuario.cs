53b4c277950e89070f2454131fd4334189b21933
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-02-24 23:11:07 -0300

    Create Usuario.cs

---
+++ ImplicitOperator/ImplicitOperator/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace ImplicitOperator
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string UsuarioNome { get; set; }
+        public string Senha { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
