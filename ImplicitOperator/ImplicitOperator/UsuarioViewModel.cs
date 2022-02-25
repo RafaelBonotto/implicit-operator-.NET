b0faae0b0653805e305e4bccf905a0a1ed04d933
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-02-24 23:11:13 -0300

    Create UsuarioViewModel.cs

---
+++ ImplicitOperator/ImplicitOperator/UsuarioViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace ImplicitOperator
+{
+    public class UsuarioViewModel
+    {
+        public UsuarioViewModel(int idUsuario, string usuarioNome)
+        {
+            UsuarioNome = usuarioNome;
+            IdUsuario = idUsuario;
+        }
+        public int IdUsuario { get; }
+        public string UsuarioNome { get; }
+
+        /// <summary>
+        /// Método responsável por receber um objeto Usuario 
+        /// e converter para um UsuarioViewModel, ocultando as demais propriedades
+        /// </summary>
+        /// <param name="usuario"></param>
+        /// 
+        public static implicit operator UsuarioViewModel(Usuario usuario)
+            => new (usuario.Id, usuario.UsuarioNome);
+    }
+}
