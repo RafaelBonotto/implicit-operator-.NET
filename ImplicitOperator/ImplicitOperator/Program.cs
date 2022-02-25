0e7c312b25af51f949eddc436ab25c093b899de2
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-02-24 23:11:17 -0300

    Create Program.cs

---
+++ ImplicitOperator/ImplicitOperator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ImplicitOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Cria uma instância de um usuário
+            var usuario = new Usuario
+            {
+                Id = 1,
+                UsuarioNome = "Rafael",
+                Senha = "senha1234",
+                Ativo = true
+            };
+
+            // Cria uma instância do ViewModel
+            // Recebe um usuário (Conversão implícita)
+            UsuarioViewModel viewModel = usuario;
+
+            Console.WriteLine("Implicit Operator Exemplo:");
+            Console.WriteLine($"Id: {viewModel.IdUsuario}");
+            Console.WriteLine($"Nome: {viewModel.UsuarioNome}");
+        }
+    }
+}
